3301732c2413a5cf976ff4867fb8045e6d672770
Justin McCandless <jmccandless@google.com>
2024-02-08 13:35:20 -0800

    I think I was playing around with a shortcuts sample

---
+++ shortcuts_intents_actions/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/jmccandless/Projects/samples/shortcuts_intents_actions/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Users/jmccandless/Projects/samples/shortcuts_intents_actions/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
---
+++ shortcuts_intents_actions/build/macos/Build/Intermediates.noindex/Pods.build/Debug/url_launcher_macos.build/Objects-normal/arm64/url_launcher_macos-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/jmccandless/Projects/samples/shortcuts_intents_actions/macos/Pods/Target\ Support\ Files/url_launcher_macos/url_launcher_macos-dummy.m \
+  /Users/jmccandless/Projects/samples/shortcuts_intents_actions/build/macos/Build/Intermediates.noindex/Pods.build/Debug/url_launcher_macos.build/module.modulemap \
+  /Users/jmccandless/Projects/samples/shortcuts_intents_actions/macos/Pods/Target\ Support\ Files/url_launcher_macos/url_launcher_macos-prefix.pch
